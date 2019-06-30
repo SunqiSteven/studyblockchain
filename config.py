187b5f55b9acbbdec4bcc661a9d1f95525523113
sunqi <1300239518@qq.com>
2019-06-30 14:41:37 +0800

    44

--- config.py
+++
@@ -1,6 +0,0 @@
-
-
-config = {
-    'version': 'v1.0.0',
-    'server_port': 83130
-}
--- exceptions.py
+++
@@ -1,6 +0,0 @@
-
-class VMError(Exception):
-    """
-        raised when vm execution error  
-    """
-    pass
