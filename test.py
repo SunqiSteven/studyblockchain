0a55c292cc7784addde5289df7502c9cb30b8c1d
sunqi <1300239518@qq.com>
2019-06-30 14:31:58 +0800

    init commit

--- test.py
+++ test.py
@@ -1,114 +1,191 @@
 
-import asyncio
+import hashlib
 import time
+import json
+
 import _thread
-import random
 
-loop = asyncio.get_event_loop()
-async def hello(future):
-    await asyncio.sleep(3)
-    # await time.sleep(3)
-    print("hello")
-    # loop.stop()
-    # future.set_result('hello result')
-    future.set_exception(Exception('22')) 
+from aiohttp import web
 
-async def foo():
-    print('foo')
+def hash_sha256(data_bytes):
+    h = hashlib.sha256()
+    h.update(data_bytes)
+    return h.hexdigest()
 
-async def slow_operation(future):
-    await asyncio.sleep(1)
-    future.set_result('Future is done!')
+class CREATE_TX_ERROR(Exception):
+    pass    
 
-def got_result(future):
-    try:
-        future.result()
-    except:
-        print(future.exception())
-    # loop.stop()
+"""
+    block
+"""
+class Block():
+    def __init__(self,timestamp,merkle_root,previous_block,nonce,trx,block_hash):
+        self.timestamp = timestamp
+        self.merkle_root = merkle_root
+        self.nonce = nonce
+        self.previous_block = previous_block
+        self.trx = trx
+        self.block_hash = block_hash
+    @classmethod
+    async def getBlockByHeight(self,request):
+        current_blocks_len = len(BLOCK_CHAIN)
+        height = request.match_info.get('height',current_blocks_len-1)
+        height = int(height)
+        if height > current_blocks_len -1:
+            rspData = {'success':'fail','msg':'not exits'}
+        else:
+            rspData = BLOCK_CHAIN[height]
+        resp = web.json_response(rspData)
+        return resp
 
-# def create():
-#     time.sleep(5)
-#     future = asyncio.Future()
-#     asyncio.ensure_future(slow_operation(future))
-#     future.add_done_callback(got_result)
+"""
+    Transaction
+"""
+class Tx():
+    def __init__(self,from_address,to_address,value):
+        self.from_address = from_address
+        self.to_address = to_address
+        self.value = value
+        self.timestamp = round(time.time())
+        # self.txHash = self.getTxHash()
+    def getTxHash(self):
+        return hash_sha256((self.from_address \
+            + self.to_address \
+            + str(self.timestamp) \
+            + str(self.value)).encode('utf-8'))
+    def toJson(self):
+        tx_dict = {
+            'from_address':self.from_address,
+            'to_address':self.to_address,
+            'value': self.value
+        }
+        return json.dumps(tx_dict)
+    @classmethod
+    def calc_merkle_root(self,trx):
+        if len(trx) == 0:
+            return ''
+        if len(trx) % 2 != 0:
+            trx.append(trx[len(trx) - 1])
+        while len(trx) > 1:
+            temp = []
+            for i in range(len(trx)):
+                if i % 2 == 0 and i+1 < len(trx):
+                    _hash = hash_sha256((trx[i] + trx[i+1]).encode('utf-8'))
+                    temp.append(_hash)
+            if len(temp) % 2 != 0 and len(temp) > 1:
+                temp.append(temp[-1])      
+            trx = temp
+        return trx[0]
+    @classmethod
+    async def  create_tx(self,request):
+        data = await request.content.read()
+        try:
+            trx_data = json.loads(data.decode('utf-8'))
+        except:
+            return web.json_response({'status':"fail",'msg':'params format error'})
+        try:
+            assert 'from_address' in trx_data,'unknow from_address'
+            assert 'to_address' in trx_data,'unknow to_address'
+            assert 'value' in trx_data,'unknow value'
+            assert trx_data['value'] < 1000 , 'invalid value'
+        except AssertionError as err:
+            raise CREATE_TX_ERROR(err)
+        tx = Tx(trx_data['from_address'],trx_data['to_address'],trx_data['value'])
+        txHash = tx.getTxHash()
+        TX_POOL[txHash] = Tx
+        respData = {'status':'success','txHash':txHash}
+        resp = web.json_response(respData)
+        return resp
 
-# _thread.start_new_thread(create,())
+'''
+    Address
+'''
+class Address():
+    def __init__(self,balance,nonce):
+        self.balance = balance
+        self.nonce = nonce
 
-# future = asyncio.Future()
-# asyncio.ensure_future(slow_operation(future))
-# future.add_done_callback(got_result)
-# print('111')
-# future = asyncio.Future()
-# asyncio.ensure_future(hello(future))
-# future.add_done_callback(got_result)
-# print(random.randrange(2**256-1))
-# print(random.random())
+MINE_DIFFICULTY = 0x01* (2 ** (8*(6-3)))
+TX_POOL = {}
+BLOCK_CHAIN = []
+GENESIS_BLOCK_HASH = '00' * 32
+"""
+    mine Block
+"""
+def mine():
+    timestamp = round(time.time())
+    trx = []
+    print(TX_POOL,'TX_POOL')
+    for k in TX_POOL:
+        trx.append(k)   
+    merkle_root = Tx.calc_merkle_root(trx)
+    previous_block = BLOCK_CHAIN[-1]['block_hash']
+    nonce = 0
+    target = 2**256 / MINE_DIFFICULTY
+    while (1):
+        headerData = str(timestamp) \
+                + merkle_root \
+                + previous_block \
+                + str(nonce)
+        blockHash = hash_sha256(headerData.encode('utf-8'))
+        if int(blockHash,16) < target:
+            print('mined ' + blockHash)
+            block_dict = {
+                'timestamp': timestamp,
+                'merkle_root':merkle_root,
+                'previous_block':previous_block,
+                'trx': trx,
+                'nonce':nonce,
+                'block_hash':blockHash
+            }
+            BLOCK_CHAIN.append(block_dict)
+            for t in trx:
+                if t in TX_POOL:
+                    del TX_POOL[t]
+            break;
+        else:
+            nonce += 1
+    mine()
 
-# try:
-#     loop.run_forever()
-# finally:
-#     loop.close()
+#
+# tx_1 = Tx('0x123456789','0x987654321',100) 
+# tx_2 = Tx('0x123456789','0x987654321',100) 
+# tx_3 = Tx('0x123456789','0x987654321',100) 
+# tx_4 = Tx('0x123456789','0x987654321',100)
 
-# def pool(size):
-#     assert size > 10 and size < 20,'invalid size'
-#     print('pool logic')
+def create_genesis_block():
+    timestamp = time.time()
+    trx = []
+    merkle_root = '00' * 32
+    previous_block = '0000'
+    nonce = 0
+    block_hash = hash_sha256((str(timestamp) \
+        + merkle_root + previous_block + str(nonce)).encode('utf-8'))
+    # return Block(timestamp,merkle_root,previous_block,nonce,trx,block_hash)
+    block = {
+        'timestamp': timestamp,
+        'merkle_root':merkle_root,
+        'previous_block':previous_block,
+        'trx': trx,
+        'nonce':nonce,
+        'block_hash':block_hash
+    }
+    BLOCK_CHAIN.append(block)
 
+def server():
+    app = web.Application()
+    app.add_routes([
+        web.post('/trx/add',Tx.create_tx),
+        web.get('/blockheight/{height}',Block.getBlockByHeight)
+    ])
+    web.run_app(app)
 
-# try:
-#     pool(100)
-# except AssertionError as err:
-#     print(err.message)
-
-
-
-import aiohttp
-
-# session = aiohttp.ClientSession()
-# time.sleep(3)
-# await session.close()
-# async with aiohttp.ClientSession() as session:
-#     async with session.get("http://127.0.0.1:8080") as resp:
-#         print(resp.status)
-#         print(await resp.text())
-
-# with open('./version.py') as fd:
-    # print(fd.read(2))
-# async def request():
-#     session = aiohttp.ClientSession() 
-#     # print(session)
-#     resp = await ClientSession.get("http://127.0.0.1:8080")
-#     print(resp.status)
-#     text = await resp.text()
-#     print(text)
-#     await session.close()
-
-
-# loop.run_until_complete(request())
+if __name__ == '__main__':
+    create_genesis_block()
+    _thread.start_new_thread(mine,())
+    server()
 
 
-# print('sunqi {}'.format('sunqi'))
-
-# with open('./keystore.txt','wb') as fd:
-#     fd.write('world'.encode('utf-8'))
-#     fd.write('hello'.encode('utf-8'))
-
-
-# async def r():
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get('http://httpbin.org/get') as resp:
-#             print(resp.status)
-#             print(await resp.text())
-# import time
-# async def foo():
-#     # asyncio.sleep(2)
-#     time.sleep(3)
-#     print('323')
-#     return '22'
-# try:
-#     foo().send(None)
-# except StopIteration as err:
-#     print(err.value)
 
 
 
